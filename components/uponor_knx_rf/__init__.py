b7d0bc5afe8e873b812c7beeabc797188e59a16c
Ricardo Schluter <ricardoschluter@hotmail.com>
2025-09-12 14:52:00 +0200

    Stage

--- components/uponor_knx_rf/__init__.py
+++ components/uponor_knx_rf/__init__.py
@@ -1,10 +1,9 @@
-# components/uponor_knx_rf/__init__.py
-import esphome.codegen as cg
+ï»¿import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import spi
 from esphome.const import CONF_ID
 
-CODEOWNERS = ["@yourhandle"]
+CODEOWNERS = ["@ricardoschluter"]
 DEPENDENCIES = ["spi"]
 AUTO_LOAD = ["logger"]
 
@@ -14,43 +13,48 @@ UponorKnxRF = ns.class_("UponorKnxRF", cg.Component, spi.SPIDevice)
 CONF_CS_PIN   = "cs_pin"
 CONF_GDO0_PIN = "gdo0_pin"
 CONF_GDO2_PIN = "gdo2_pin"
-CONF_RST_PIN  = "rst_pin"  # optional
+CONF_RST_PIN  = "rst_pin"
+CONF_FREQUENCY = "frequency"
+CONF_THERMOSTATS = "thermostats"
 
-CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(UponorKnxRF),
+CONFIG_SCHEMA = (
+    cv.Schema({
+        cv.GenerateID(): cv.declare_id(UponorKnxRF),
 
-    # SPI device select (validates it’s on the configured SPI bus)
-    cv.Required(CONF_CS_PIN):  cv.internal_gpio_output_pin_schema,
+        # âœ… use generic GPIO schemas (spi_cs_pin no longer exists)
+        cv.Required(CONF_CS_PIN):   cv.internal_gpio_output_pin_schema,
+        cv.Required(CONF_GDO0_PIN): cv.internal_gpio_input_pin_schema,
+        cv.Required(CONF_GDO2_PIN): cv.internal_gpio_input_pin_schema,
+        cv.Optional(CONF_RST_PIN):  cv.internal_gpio_output_pin_schema,
 
-    # GDO lines as inputs (internal schema enables pullup/interrupt options)
-    cv.Required(CONF_GDO0_PIN): cv.internal_gpio_input_pin_schema,
-    cv.Required(CONF_GDO2_PIN): cv.internal_gpio_input_pin_schema,
-
-    # Optional reset pin (many CC1101 boards don’t break this out)
-    cv.Optional(CONF_RST_PIN): cv.internal_gpio_output_pin_schema,
-
-    # (… your other options …)
-}).extend(cv.COMPONENT_SCHEMA).extend(spi.spi_device_schema())
+        cv.Optional(CONF_FREQUENCY, default=868_300_000): cv.frequency,
+        cv.Optional(CONF_THERMOSTATS, default=[]): cv.ensure_list(cv.HexInt),
+    })
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(spi.spi_device_schema())
+)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await spi.register_spi_device(var, config)  # binds to the YAML `spi:` bus
+    await spi.register_spi_device(var, config)
 
-    cs = await cg.gpio_pin_expression(config[CONF_CS_PIN])
+    cs  = await cg.gpio_pin_expression(config[CONF_CS_PIN])
+    g0  = await cg.gpio_pin_expression(config[CONF_GDO0_PIN])
+    g2  = await cg.gpio_pin_expression(config[CONF_GDO2_PIN])
     cg.add(var.set_cs_pin(cs))
-
-    g0 = await cg.gpio_pin_expression(config[CONF_GDO0_PIN])
     cg.add(var.set_gdo0_pin(g0))
-
-    g2 = await cg.gpio_pin_expression(config[CONF_GDO2_PIN])
     cg.add(var.set_gdo2_pin(g2))
 
     if CONF_RST_PIN in config:
         rst = await cg.gpio_pin_expression(config[CONF_RST_PIN])
         cg.add(var.set_rst_pin(rst))
 
-    # If you use RadioLib:
+    cg.add(var.set_frequency(config[CONF_FREQUENCY]))
+    for tid in config[CONF_THERMOSTATS]:
+        cg.add(var.add_thermostat_id(tid))
+
+    # Pull RadioLib automatically
     cg.add_library(
         name="RadioLib",
         repository="https://github.com/jgromes/RadioLib",
